b03469c488bab914018bf538723ee67eb5b76ae1
young <ly532265997@gmail.com>
2017-05-27 15:52:18 +0800

    打包文件

--- debug/simplemde.js
+++ debug/simplemde.js
@@ -1,7 +1,7 @@
 /**
- * simplemde v1.11.2
- * Copyright Next Step Webs, Inc.
- * @link https://github.com/NextStepWebs/simplemde-markdown-editor
+ * simarkdown v1.0.0
+ * Copyright luo yang
+ * @link https://github.com/youngluo/simarkdown
  * @license MIT
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.SimpleMDE = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
@@ -15543,7 +15543,7 @@ if (!CodeMirror.mimeModes.hasOwnProperty("text/html"))
 function getError(url, option, xhr) {
     var msg;
     if (xhr.response) {
-        msg = xhr.status + ' ' + (xhr.response.error || xhr.response);
+        msg = xhr.status + ' ' + (xhr.response.message || xhr.response);
     } else if (xhr.responseText) {
         msg = xhr.status + ' ' + xhr.responseText;
     } else {
@@ -16295,12 +16295,6 @@ function drawLink(editor) {
 	var stat = getState(cm);
 	var options = editor.options;
 	var url = "http://";
-	// if(options.promptURLs) {
-	// 	url = prompt(options.promptTexts.link);
-	// 	if(!url) {
-	// 		return false;
-	// 	}
-	// }
 	_replaceSelection(cm, stat.link, options.insertTexts.link, url);
 }
 
